b4f5cd3a6e590efdabd09bc1c9b143f1a9974150
Adeniyi Tofunmi <Adeniyitofunmi@gmail.com>
2022-01-05 08:51:58 +0100

    bug fixed

--- src/router/index.js
+++ src/router/index.js
@@ -46,7 +46,7 @@ const routes = [
     },
   },
   {
-    path: "/sinup",
+    path: "/signup",
     name: "Signup",
     component: () =>
       import(/* webpackChunkName: "Login" */ "../views/Signup.vue"),
